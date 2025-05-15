24b1c8029b414775aec0cef93954eb18ac8991b3
GaetanoNicotra <nicotragaetano10@gmail.com>
2025-05-15 16:04:20 +0200

    [UPD] definizione delle rotte in App.jsx e strutturadella navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,25 @@
 import { useState } from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Homepage from './pages/Homepage'
+import About from './pages/About'
+import Contacts from './pages/contacts'
+import Products from './pages/Products'
 
 
 function App() {
 
+  const [currentPage, setCurrentPage] = useState('Home')
 
   return (
     <>
-
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" Component={Homepage} ></Route>
+          <Route path="/about" Component={About} ></Route>
+          <Route path="/contacts" Component={Contacts} ></Route>
+          <Route path="/products" Component={Products} ></Route>
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
