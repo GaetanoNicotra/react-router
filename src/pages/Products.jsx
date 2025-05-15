745580376f44f69a4bada325383c4fa4d41cafb9
GaetanoNicotra <nicotragaetano10@gmail.com>
2025-05-15 16:24:19 +0200

    [UPD] creazione della componente Navbar.jsx

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -1,28 +1,9 @@
 import React from 'react'
-import { NavLink } from 'react-router-dom'
+import Navbar from '../components/Navbar'
 const Products = () => {
     return (
         <>
-            <nav className="navbar navbar-expand-lg bg-body-tertiary">
-                <div className="container-fluid">
-                    <div className="collapse navbar-collapse" id="navbarNavDropdown">
-                        <ul className="navbar-nav">
-                            <li className="nav-item ">
-                                <NavLink to='/'>HOMEPAGE</NavLink>
-                            </li>
-                            <li className="nav-item">
-                                <NavLink to='/about'>ABOUT</NavLink>
-                            </li>
-                            <li className="nav-item">
-                                <NavLink to='/contacts'>CONTACTS</NavLink>
-                            </li>
-                            <li className="nav-item">
-                                <NavLink to='/products'>PRODUCTS</NavLink>
-                            </li>
-                        </ul>
-                    </div>
-                </div>
-            </nav>
+            <Navbar />
 
             <div>
                 <h2>PRODOTTI</h2>
