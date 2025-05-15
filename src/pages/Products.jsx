b789b93d84c53f5df60cbaaabbed431486d6136f
GaetanoNicotra <nicotragaetano10@gmail.com>
2025-05-15 17:39:30 +0200

    [UPD] generazione delle card tramite chiamta ajax

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -9,7 +9,7 @@ const Products = () => {
 
     const getProducts = () => {
         axios.get('https://dummyjson.com/recipes/').then((resp) => {
-            setProducts(resp.data);
+            setProducts(resp.data.recipes);
         });
     };
 
@@ -22,13 +22,28 @@ const Products = () => {
             <Navbar />
             <div className="container">
                 <div className="row">
-                    <div className="col-12">
+                    <div className="col-3">
                         <h1>LISTA PRODOTTI</h1>
+                        {products.map((product) => {
+                            return (
+                                <div className="card">
+                                    <img src={product.image} className="card-img-top" alt="..." />
+                                    <div className="card-body">
+                                        <h5 className="card-title">{product.name}</h5>
+                                        <p className="card-text">{product.ingredients}</p>
+                                    </div>
+                                    <ul className="list-group list-group-flush">
+                                        <li className="list-group-item"> {product.difficulty}</li>
+                                        <li className="list-group-item">{product.cuisine}</li>
+                                        <li className="list-group-item">A third item</li>
+                                    </ul>
+                                </div>
+                            );
+                        })};
                     </div>
                 </div>
-            </div>
+            </div >
         </>
-
     )
 }
 
