485133f9b06a50e0b583cda3135dbaf38acf7c28
GaetanoNicotra <nicotragaetano10@gmail.com>
2025-05-15 16:38:04 +0200

    [UPD] generazione dei link della navbar tramite l'utilizzo dle map

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,23 +1,34 @@
 import React from 'react'
 import { NavLink } from 'react-router-dom'
+
+const links = [
+    { path: '/', label: "Homepage" },
+    { path: '/about', label: "About" },
+    { path: '/contacts', label: "Contacts" },
+    { path: '/products', label: "Products" }
+
+]
+
 const Navbar = () => {
+
     return (
         <nav className="navbar navbar-expand-lg bg-body-tertiary">
             <div className="container-fluid">
                 <div className="collapse navbar-collapse" id="navbarNavDropdown">
-                    <ul className="navbar-nav">
-                        <li className="nav-item">
-                            <NavLink to='/'>HOMEPAGE</NavLink>
-                        </li>
-                        <li className="nav-item">
+                    <ul className="navbar-nav ">
+                        {links.map((link, index) => {
+                            return <li key={index} className="nav-item me-4"> <NavLink to={link.path}>{link.label}</NavLink></li>
+                        })}
+
+                        {/* <li className="nav-item me-4">
                             <NavLink to='/about'>ABOUT</NavLink>
                         </li>
-                        <li className="nav-item">
+                        <li className="nav-item me-4">
                             <NavLink to='/contacts'>CONTACTS</NavLink>
                         </li>
-                        <li className="nav-item">
+                        <li className="nav-item me-4">
                             <NavLink to='/products'>PRODUCTS</NavLink>
-                        </li>
+                        </li> */}
                     </ul>
                 </div>
             </div>
