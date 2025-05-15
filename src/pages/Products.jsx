7324210d5bde79ed6bf0c678c1a494c6199f9f23
GaetanoNicotra <nicotragaetano10@gmail.com>
2025-05-15 18:05:00 +0200

    [UPD] stilizzazione card

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -21,26 +21,25 @@ const Products = () => {
         <>
             <Navbar />
             <div className="container">
+                <h1 className='mb-4' >LISTA PRODOTTI</h1>
                 <div className="row">
-                    <div className="col-3">
-                        <h1>LISTA PRODOTTI</h1>
-                        {products.map((product) => {
-                            return (
-                                <div className="card">
-                                    <img src={product.image} className="card-img-top" alt="..." />
-                                    <div className="card-body">
-                                        <h5 className="card-title">{product.name}</h5>
-                                        <p className="card-text">{product.ingredients}</p>
-                                    </div>
-                                    <ul className="list-group list-group-flush">
-                                        <li className="list-group-item"> {product.difficulty}</li>
-                                        <li className="list-group-item">{product.cuisine}</li>
-                                        <li className="list-group-item">A third item</li>
-                                    </ul>
+                    {products.map((product) => {
+                        return (
+                            <div className="card col-3 mb-4 g-2">
+                                <img src={product.image} className="card-img-top" alt="..." />
+                                <div className="card-body">
+                                    <h5 className="card-title">{product.name}</h5>
+                                    <p className="card-text">{product.ingredients.join(',')}</p>
                                 </div>
-                            );
-                        })};
-                    </div>
+                                <ul className="list-group list-group-flush">
+                                    <li className="list-group-item"> {product.difficulty}</li>
+                                    <li className="list-group-item">{product.cuisine}</li>
+                                    <li className="list-group-item">A third item</li>
+                                </ul>
+                            </div>
+                        );
+                    })};
+
                 </div>
             </div >
         </>
