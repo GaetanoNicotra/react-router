63805fb1b449c19a55d10ef349a3c8d1fbde059c
GaetanoNicotra <nicotragaetano10@gmail.com>
2025-05-16 17:53:56 +0200

    [UPD] funzione per il recupero del dettaglio tramite ajax

--- src/pages/prodotti/DetailProduct.jsx
+++ src/pages/prodotti/DetailProduct.jsx
@@ -1,14 +1,36 @@
 import React from 'react'
 import { useParams } from 'react-router-dom'
-
+import axios from 'axios';
+import { useEffect } from 'react';
 
 const DetailProduct = () => {
     const params = useParams();
     const id = params.id
     console.log(params)
+
+    const [product, setProduct] = useState({})
+
+    const getSingleProduct = () => {
+        axios.get(`https://dummyjson.com/recipes/${id}`).then((resp) => {
+            setProduct(resp.data)
+        })
+    }
+
+    useEffect(() => {
+        getSingleProduct();
+    }, [])
+
     return (
         <div>
             <h1>DETTAGLIO DEL PRODOTTO:</h1>
+            <div className="container">
+                <div className="row">
+                    <div className="col-12">
+                        <h1>{product.name}</h1>
+                        <img src={product.image} alt="" />
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
