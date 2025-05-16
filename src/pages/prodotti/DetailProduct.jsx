c515001680c1d6effc0337b82aab2279bdcff17d
GaetanoNicotra <nicotragaetano10@gmail.com>
2025-05-16 15:33:45 +0200

    [UPD]

--- src/pages/prodotti/DetailProduct.jsx
+++ src/pages/prodotti/DetailProduct.jsx
@@ -1,6 +1,9 @@
 import React from 'react'
+import { useParams } from 'react-router-dom'
+
 
 const DetailProduct = () => {
+    const params = useParams();
     return (
         <div>
             <h1>DETTAGLIO DEL PRODOTTO:</h1>
